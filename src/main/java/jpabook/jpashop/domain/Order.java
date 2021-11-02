6fc15eaf03ca90ba1e6114111395708468f8ae6b
yeonjiyeon <dalnim95@naver.com>
2021-11-02 21:21:05 +0900

    ok

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -73,7 +73,7 @@ public class Order {
         if (delivery.getStatus() == DeliveryStatus.COMP){
             throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다");
         }
-        this.setStatus(OrderStatus.CENCEL);
+        this.setStatus(OrderStatus.CANCEL);
         for (OrderItem orderItem : orderItems) {
             orderItem.cancel();
         }
