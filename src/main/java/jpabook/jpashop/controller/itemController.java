197f4c5b058ab6c8b34b4eeb1525d348d6a52242
yeonjiyeon <dalnim95@naver.com>
2021-11-06 13:55:15 +0900

    ok

--- src/main/java/jpabook/jpashop/controller/itemController.java
+++ src/main/java/jpabook/jpashop/controller/itemController.java
@@ -44,17 +44,24 @@ public class itemController {
         return "items/itemList";
     }
 
+//    @GetMapping(value = "/items/{itemId}/edit")
+//    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+//        Book item = (Book) itemService.findOne(itemId);
+//        BookForm form = new BookForm();
+//        form.setId(item.getId());
+//        form.setName(item.getName());
+//        form.setPrice(item.getPrice());
+//        form.setStockQuantity(item.getStockQuantity());
+//        form.setAuthor(item.getAuthor());
+//        form.setIsbn(item.getIsbn());
+//        model.addAttribute("form", form);
+//        return "items/updateItemForm";
+//    }
+
     @GetMapping(value = "/items/{itemId}/edit")
-    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
-        Book item = (Book) itemService.findOne(itemId);
-        BookForm form = new BookForm();
-        form.setId(item.getId());
-        form.setName(item.getName());
-        form.setPrice(item.getPrice());
-        form.setStockQuantity(item.getStockQuantity());
-        form.setAuthor(item.getAuthor());
-        form.setIsbn(item.getIsbn());
-        model.addAttribute("form", form);
+    public String updateItemForm(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+
         return "items/updateItemForm";
     }
 
@@ -68,6 +75,7 @@ public class itemController {
         book.setStockQuantity(form.getStockQuantity());
         book.setAuthor(form.getAuthor());
         book.setIsbn(form.getIsbn());
+
         itemService.saveItem(book);
         return "redirect:/items";
 
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,23 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+    
+    @Transactional
+//    public Item updateItem(Long itemId, Book param){
+//        Item findItem = itemRepository.findOne(itemId);
+//        findItem.setPrice(param.getPrice());
+//        findItem.setName(param.getName());
+//        findItem.setStockQuantity(param.getStockQuantity());
+//        return findItem;
+//    }
+
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        return findItem;
     }
 
     public List<Item> findItems(){
